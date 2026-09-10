2611f291cb0c4ca4fd482de8f30fe10c013a29fc
Taleysha <132191568+Taleysha@users.noreply.github.com>
2026-09-10 17:35:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import flasked
+import flaskededed
 from flask import Flask, render_template, request, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
